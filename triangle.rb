dfe6f606378d2a2a663d250e644b3835c2aad7ad
KarenVentura <ventura.802@hotmail.com>
2015-07-09 12:18:34 -0500

    Test about_method_of_triangle

--- triangle.rb
+++ triangle.rb
@@ -15,8 +15,14 @@
 #
 def triangle(a, b, c)
   # WRITE THIS CODE
+ if a==b and a==c and c==b
+	return :equilateral
+ elsif a!=b and a!=c and b!=c
+ 	return :scalene
+ else
+	return :isosceles
+ end
 end
-
 # Error class used in part 2.  No need to change this code.
 class TriangleError < StandardError
 end
